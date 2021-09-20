8fb67e9c8e484c941d2d62d91d6c7343d65b1b39
AdamKMarks <86434397+AdamKMarks@users.noreply.github.com>
2021-09-20 17:51:38 -0500

    Update vm2.tf

--- vm2.tf
+++ vm2.tf
@@ -2,6 +2,6 @@
 resource "azurerm_image" "vm" {
   name                             = "AZLXDEVOPS01"
   location                         = local.region
-  resource_group_name              = "myTFResourceGroup"
+  resource_group_name              = azurerm_resource_group.rg.name
   source_virtual_machine_id        = "/subscriptions/ed5e2254-5d87-4255-b70e-1b5eba509f73/resourceGroups/app-imagegallery-prod-eastus2/providers/Microsoft.Compute/images/Windows2019-DC-LTS-daily-1618388082"
 }
