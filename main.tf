34cf1d7f519e524b24b3444f33c0570463eed89c
AdamKMarks <86434397+AdamKMarks@users.noreply.github.com>
2021-09-20 10:23:36 -0500

    Update main.tf
    Add security groups

--- main.tf
+++ main.tf
@@ -47,3 +47,14 @@ resource "azurerm_subnet" "lab-private" {
   virtual_network_name = azurerm_virtual_network.lab.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "lab-public" {
+  name                = "aztf-labs-public-sg"
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "lab-public" {
+  subnet_id                 = azurerm_subnet.lab-public.id
+  network_security_group_id = azurerm_network_security_group.lab-public.id
+}
