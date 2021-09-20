fb2e7d429735a4d75fa6d6c9ca738f480ef0fa9a
AdamKMarks <86434397+AdamKMarks@users.noreply.github.com>
2021-09-20 17:07:36 -0500

    Create vm2.tf

---
+++ vm2.tf
@@ -0,0 +1,7 @@
+# Create a new Virtual Machine based on the Golden Image
+resource "azurerm_image" "vm" {
+  name                             = "AZLXDEVOPS01"
+  location                         = local.region
+  resource_group_name              = "myTFResourceGroup"
+  source_virtual_machine_id        = "/subscriptions/ed5e2254-5d87-4255-b70e-1b5eba509f73/resourceGroups/app-imagegallery-prod-eastus2/providers/Microsoft.Compute/images/Windows2019-DC-LTS-daily-1618388082"
+}
